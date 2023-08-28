afa51177aa5f9db961bff04a0ae06d7632ad3eee
arturokunder <arturokunder@users.noreply.github.com>
2023-08-28 16:38:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+login = False
+
 @app.route('/')
 def hello_world():
     return 'Hello, World! 3'
+
+@app.route('/login')
+def login():
+    login = True
+    return 'Logged in'
+
+@app.route('/secret')
+def secret():
+    if login:
+        return 'secret!!'
+    return 'Forbidden'
